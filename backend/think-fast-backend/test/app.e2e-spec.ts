275c9e64f25d332bf50f15e5e663087b4cac76db
Daniel Agmon <daniel.agmon@gmail.com>
2023-03-08 15:41:54 +0200

    Fix backend e2e health check

--- backend/think-fast-backend/test/app.e2e-spec.ts
+++ backend/think-fast-backend/test/app.e2e-spec.ts
@@ -15,10 +15,11 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  it('/health-check (GET)', async () => {
+    const res = await request(app.getHttpServer())
+      .get('/health-check')
+      .expect(200);
+
+    expect(res.body.message === 'healthy');
   });
 });
